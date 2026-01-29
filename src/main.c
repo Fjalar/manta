122510796ed031c832d7a8ec53e3ef1159713c7b
Fjalar <fjalarhognason@gmail.com>
2026-01-29 13:48:52 +0100

    Rainbow triangle

--- src/main.c
+++ src/main.c
@@ -18,24 +18,97 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     GLFWwindow *window = glfwCreateWindow(512, 512, "Manta", NULL, NULL);
     glfwMakeContextCurrent(window);
-    int version = gladLoadGL(glfwGetProcAddress);
-    printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+    int glad_version = gladLoadGL(glfwGetProcAddress);
+    printf("GL %d.%d\n", GLAD_VERSION_MAJOR(glad_version), GLAD_VERSION_MINOR(glad_version));
 
-    int major;
-    int minor;
-    int rev;
+    int glfw_major, glfw_minor, glfw_rev;
+    glfwGetVersion(&glfw_major, &glfw_minor, &glfw_rev);
+    printf("GLFW %d.%d.%d\n", glfw_major, glfw_minor, glfw_rev);
 
-    glfwGetVersion(&major, &minor, &rev);
-    printf("GLFW %d.%d.%d\n", major, minor, rev);
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    glClearColor(0.3, 0.7, 0.3, 1.0);
+
+    float points[] = {
+       0.0f,  0.5f,  0.0f, // x,y,z of first point.
+       0.5f, -0.5f,  0.0f, // x,y,z of second point.
+      -0.5f, -0.5f,  0.0f  // x,y,z of third point.
+    };
+
+    float colours[] = {
+      1.0f, 0.0f,  0.0f,
+      0.0f, 1.0f,  0.0f,
+      0.0f, 0.0f,  1.0f
+    };
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+    GLuint colours_vbo = 0;
+    glGenBuffers(1, &colours_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colours_vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), colours, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glBindBuffer(GL_ARRAY_BUFFER, colours_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+    const char* vertex_shader =
+    "#version 410 core\n"
+    "layout(location = 0) in vec3 vertex_position;"
+    "layout(location = 1) in vec3 vertex_colour;"
+    "out vec3 colour;"
+    "void main() {"
+    "   colour = vertex_colour;"
+    "   gl_Position = vec4(vertex_position, 1.0);"
+    "}";
+
+    const char* fragment_shader =
+    "#version 410 core\n"
+    "in vec3 colour;"
+    "out vec4 frag_colour;"
+    "void main() {"
+    "   frag_colour = vec4(colour, 1.0);"
+    "}";
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, fs);
+    glAttachShader(shader_program, vs);
+    glLinkProgram(shader_program);
 
     glfwSetKeyCallback(window, quit);
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
-        glClearColor(0.3, 0.7, 0.3, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
     }
 
